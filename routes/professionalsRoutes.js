ce9413b52baa5d60832770578a5b78e23f982f17
Dilip <dstha0304@gmail.com>
2026-06-30 20:41:12 +0545

    Implement MongoDB with Mongoose

---
+++ routes/professionalsRoutes.js
@@ -0,0 +1,120 @@
+const express = require("express");
+
+const router = express.Router();
+const professionals = require("../data/professionals");
+
+// GET ALL PROFESSIONALS
+
+router.get("/", (req, res) => {
+
+    const category = req.query.category;
+
+    if (category) {
+
+        const filtered = professionals.filter(
+            p => p.category === category
+        );
+
+        return res.status(200).json(filtered);
+    }
+
+    res.status(200).json(professionals);
+});
+
+
+// GET PROFESSIONAL BY ID
+
+router.get("/:id", (req, res) => {
+
+    const targetId = parseInt(req.params.id);
+
+    const result = professionals.find(
+        p => p.id === targetId
+    );
+
+    if (!result) {
+        return res.status(404).json({
+            error: "Professional not found"
+        });
+    }
+
+    res.status(200).json(result);
+});
+
+
+// CREATE PROFESSIONAL
+
+router.post("/", (req, res) => {
+
+    const { name, category } = req.body;
+
+    if (!name || !category) {
+        return res.status(400).json({
+            error: "Missing required fields"
+        });
+    }
+
+    const generatedProfile = {
+        id: professionals.length + 1,
+        name,
+        category
+    };
+
+    professionals.push(generatedProfile);
+
+    res.status(201).json(generatedProfile);
+});
+
+// UPDATE PROFESSIONAL
+
+router.put("/:id", (req, res) => {
+
+    const targetId = parseInt(req.params.id);
+
+    const { name, category } = req.body;
+
+    const professional = professionals.find(
+        p => p.id === targetId
+    );
+
+    if (!professional) {
+        return res.status(404).json({
+            error: "Professional not found"
+        });
+    }
+
+    if (name) {
+        professional.name = name;
+    }
+
+    if (category) {
+        professional.category = category;
+    }
+
+    res.status(200).json(professional);
+});
+
+// DELETE PROFESSIONAL
+
+router.delete("/:id", (req, res) => {
+
+    const targetId = parseInt(req.params.id);
+
+    const index = professionals.findIndex(
+        p => p.id === targetId
+    );
+
+    if (index === -1) {
+        return res.status(404).json({
+            error: "Professional not found"
+        });
+    }
+
+    professionals.splice(index, 1);
+
+    res.status(200).json({
+        message: "Professional deleted successfully"
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,129 +1,25 @@
 const express = require("express");
 const app = express();
+const PORT = 3000;
 
-app.use(express.json());
+
 
 const professionals = require("./data/professionals");
 
+const mongoose = require("mongoose");
 
-
-// GET ALL PROFESSIONALS
-
-app.get("/api/professionals", (req, res) => {
-
-    const category = req.query.category;
-
-    if (category) {
-
-        const filtered = professionals.filter(
-            p => p.category === category
-        );
-
-        return res.status(200).json(filtered);
-    }
-
-    res.status(200).json(professionals);
+mongoose.connect("mongodb://localhost:27017/professionalsDB").then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("Error connecting to MongoDB", err);
 });
 
+app.use(express.json());
 
-// GET PROFESSIONAL BY ID
-
-app.get("/api/professionals/:id", (req, res) => {
-
-    const targetId = parseInt(req.params.id);
-
-    const result = professionals.find(
-        p => p.id === targetId
-    );
-
-    if (!result) {
-        return res.status(404).json({
-            error: "Professional not found"
-        });
-    }
-
-    res.status(200).json(result);
-});
+const professionalRoutes = require("./routes/professionalsRoutes");
+app.use("/api/professionals", professionalRoutes);
 
 
-// CREATE PROFESSIONAL
-
-app.post("/api/professionals", (req, res) => {
-
-    const { name, category } = req.body;
-
-    if (!name || !category) {
-        return res.status(400).json({
-            error: "Missing required fields"
-        });
-    }
-
-    const generatedProfile = {
-        id: professionals.length + 1,
-        name,
-        category
-    };
-
-    professionals.push(generatedProfile);
-
-    res.status(201).json(generatedProfile);
-});
-
-// UPDATE PROFESSIONAL
-
-app.put("/api/professionals/:id", (req, res) => {
-
-    const targetId = parseInt(req.params.id);
-
-    const { name, category } = req.body;
-
-    const professional = professionals.find(
-        p => p.id === targetId
-    );
-
-    if (!professional) {
-        return res.status(404).json({
-            error: "Professional not found"
-        });
-    }
-
-    if (name) {
-        professional.name = name;
-    }
-
-    if (category) {
-        professional.category = category;
-    }
-
-    res.status(200).json(professional);
-});
-
-// DELETE PROFESSIONAL
-
-app.delete("/api/professionals/:id", (req, res) => {
-
-    const targetId = parseInt(req.params.id);
-
-    const index = professionals.findIndex(
-        p => p.id === targetId
-    );
-
-    if (index === -1) {
-        return res.status(404).json({
-            error: "Professional not found"
-        });
-    }
-
-    professionals.splice(index, 1);
-
-    res.status(200).json({
-        message: "Professional deleted successfully"
-    });
-});
-
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
