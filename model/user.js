89ba36342e7d9e396625b9175b1cdf763fdf940f
Dilip <dstha0304@gmail.com>
2026-07-08 22:50:44 +0545

    Add name field to User model

---
+++ model/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true,
+        minlength: [3, 'Name must be at least 3 characters']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        trim: true,
+        unique: true,
+        match: [/.+\@.+\..+/, 'Please fill a valid email address']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters']
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
