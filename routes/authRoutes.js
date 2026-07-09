2dce82c862c492a0d25c0a49b99f903fcc1a1ee7
Dilip <dstha0304@gmail.com>
2026-07-09 22:49:05 +0545

    Implement login routes with JWT auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -27,5 +27,36 @@ router.post("/register", async (req, res) => {
     }
 });
 
+// login existing user
+router.post("/login", async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        if (!email || !password) {
+            return res.status(400).json( {error: "All fields are required" });
+        }
+        const user = await User.findOne({email});
+        if (!user) return res.status(400).json("User not found");
+        const passMatch = await comparePass(password, user.password);
+
+        if (!passMatch) {
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
+
+        const token = jwt.sign(
+            { userId: user.id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+        // Success - User is authenticated
+        res.status(200).json({ 
+            message: "Login successful", 
+            username: user.username,
+            token: token
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({error: error.message});
+    }
+});
 
 module.exports = router;
