ed4b76be21318cb1add1f1fff63865506379ca45
Dilip <dstha0304@gmail.com>
2026-07-13 22:52:12 +0545

    Correct Indentation

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,7 +5,9 @@ const jwt = require('jsonwebtoken');
 // Middleware function to verify JWT token
 module.exports = (req, res, next) => {
     const token = req.header("x-auth-token");
+
      // Check if token is present in the request header
+
     if (!token) {
         return res.status(401).json({
             error: "No token supplied. Authorization denied."
@@ -13,11 +15,14 @@ module.exports = (req, res, next) => {
     }
     
     // Verify the token using the secret key
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
+
     // Handle invalid token errors
+    
     } catch (error) {
         console.error(error);
         res.status(401).json({error: "Token Signature is invalid"});
