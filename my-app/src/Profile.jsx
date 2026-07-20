507b70ae4b33b3388f1caf21a8e3da63c3a6bcfb
Dilip <dstha0304@gmail.com>
2026-07-20 22:58:18 +0545

    Added Profile page

---
+++ my-app/src/Profile.jsx
@@ -0,0 +1,14 @@
+// Profile.jsx
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>My Profile</h1>
+      <p>This is a simple profile page.</p>
+    </div>
+  );
+};
+
+// Export the Profile component as the default export
+
+export default Profile;
