d28bbf80d274f9e3ef684fe5f4a5b2da68d3682b
Dilip <dstha0304@gmail.com>
2026-07-23 23:12:06 +0545

    Changes in Profile

--- my-app/src/components/Profile.jsx
+++ my-app/src/components/Profile.jsx
@@ -1,6 +1,13 @@
-import { useContext } from "react"
+import { useContext } from 'react';
+import { UserContext } from '../App';
 
 export function Profile() {
-    const { user } = useContext(UserContext)
-    return <p>Welcome, {user}!</p>
+    const {user} = useContext(UserContext);
+
+    return (
+        <div>
+            <h2>User Profile</h2>
+            <p>Name: {user}</p>
+        </div>
+    );
 }
